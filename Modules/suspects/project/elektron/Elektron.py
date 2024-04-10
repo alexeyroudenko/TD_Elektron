c9198b6b9f5d03dcaa2a916794ebb6fe384d769f
Alexey Roudenko <alexey.roudenko@gmail.com>
2024-04-10 11:44:38 +0300

    component

---
+++ Modules/suspects/project/elektron/Elektron.py
@@ -0,0 +1,11 @@
+'''Info Header Start
+Name : Elektron
+Info Header End'''
+
+class Elektron:
+	"""
+	Elektron description
+	"""
+	def __init__(self, ownerComp):
+		# The component to which this extension is attached
+		self.ownerComp = ownerComp
