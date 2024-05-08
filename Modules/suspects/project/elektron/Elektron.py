c673b116cf47c3e3b052e20db92e3b862c325fd8
Alexey Roudenko <alexey.roudenko@gmail.com>
2024-05-08 14:27:21 +0300

    refactor

--- Modules/suspects/project/elektron/Elektron.py
+++ Modules/suspects/project/elektron/Elektron.py
@@ -9,3 +9,29 @@ class Elektron:
 	def __init__(self, ownerComp):
 		# The component to which this extension is attached
 		self.ownerComp = ownerComp
+		print("Elektron init done", ownerComp)
+
+	def OnMidi(self, rowIndex, message, index, value, input):
+		'''
+		MIDI Events
+		'''
+		if message == "Timing Clock":
+			pass
+		else:
+			#print(rowIndex, message, index, value, input)
+			if message == 'Start':
+				op('constant4').par.value0 = 0
+				op('constantStatus').par.value0 = 1
+			if message == 'Stop':
+				op('constant4').par.value0 = 0
+				op('constantStatus').par.value0 = 0        
+
+			if rowIndex == 101:
+				if message == "Timing Clock":
+					pass
+			if message == 'Program Change':
+				op('constantPattern').par.value0 = index - 1
+	
+	def Ping(self):
+		# print("png")
+		pass
